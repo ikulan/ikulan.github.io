3b6cff78c2b508db28a5778726ec0443d3323084
ikulan <ikulan12@gmail.com>
2024-01-24 15:13:36 -0800

    Add Avatar component
    Two sizes: `small` and `base` (default)

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,18 @@
+function Avatar({ img_url, alt_name, size = "base" }) {
+  const sizeProperties = {
+    small: "h-16 w-16 border-2",
+    base: "h-36 w-36 md:h-48 md:w-48 border-4",
+  };
+
+  return (
+    <div>
+      <img
+        className={`block text-center rounded-full border-peach-200 ${sizeProperties[size]}`}
+        src={img_url}
+        alt={alt_name}
+      />
+    </div>
+  );
+}
+
+export default Avatar;
