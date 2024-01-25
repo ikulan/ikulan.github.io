12ce1501f21b3fd8f7b49969d274038feb59df05
ikulan <ikulan12@gmail.com>
2024-01-25 11:52:41 -0800

    :wrench: Add library: flowbite-react
    Flowbite is a component library works well with Tailwind CSS. It contains many useful UI components for web development.
    
    https://www.flowbite-react.com/

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
   theme: {
     extend: {
       colors: {
@@ -46,5 +50,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
