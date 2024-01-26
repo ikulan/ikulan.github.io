39436c03e57f1108e1492714978709d582503287
ikulan <ikulan12@gmail.com>
2024-01-25 16:48:25 -0800

    Add experience section

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -9,30 +9,30 @@ function AccordionItem(props) {
         <div className="flex flex-col justify-between gap-2 gap-x-6 py-2 md:flex-row md:py-5">
           {/* title, company and period */}
           <div className="md:min-w-64">
-            <p className="text-xl font-semibold leading-6 text-stone-900">
+            <p className="text-lg font-semibold leading-6 text-stone-900 md:text-xl">
               {props.title}
             </p>
             <div>
               <a
-                className="inline-flex items-center gap-x-1 text-lg text-stone-500 hover:text-peach-600 hover:underline"
+                className="inline-flex items-center gap-x-1 text-stone-500 hover:text-peach-600 hover:underline md:text-lg"
                 href={props.companyUrl}
                 target="_blank"
                 rel="noreferrer"
               >
                 <span>{props.companyName}</span>
                 <span>
-                  <ExtLinkSvg className="h-5 w-5" />
+                  <ExtLinkSvg className="h-4 w-4 md:h-5 md:w-5" />
                 </span>
               </a>
             </div>
-            <span className="text-stone-500">
+            <span className="text-sm text-stone-500 md:text-base">
               {props.startYear} - {props.endYear}
             </span>
           </div>
 
           {/* skills */}
-          <div className="m-auto p-0">
-            <div className="flex flex-wrap gap-y-1 md:items-end">
+          <div className="p-0">
+            <div className="flex flex-wrap gap-y-1 md:gap-y-2">
               {props.skills &&
                 props.skills.map((s) => (
                   <Tag key={s.name} color={s.color}>
@@ -44,7 +44,7 @@ function AccordionItem(props) {
         </div>
       </Accordion.Title>
       <Accordion.Content>
-        <div className="min-h-28 p-3 pl-6 text-lg">
+        <div className="min-h-28 p-3 pl-6 md:text-lg">
           <ul className="list-disc">
             {props.descriptions &&
               props.descriptions.map((line, i) => (
--- src/components/ExpAccordion.jsx
+++ src/components/ExpAccordion.jsx
@@ -4,19 +4,19 @@ import AccordionItem from "./AccordionItem";
 const customTheme = {
   accordion: {
     root: {
-      base: "divide-y divide-dashed divide-stone-300 border-stone-300 shadow-lg shadow-peach-200 dark:divide-stone-700 dark:border-stone-700",
+      base: "divide-y divide-dashed divide-stone-300 border-stone-300 shadow-lg shadow-stone-300",
     },
     content: {
-      base: "px-5 last:rounded-b-lg dark:bg-gray-900 first:rounded-t-lg",
+      base: "px-5 last:rounded-b-lg first:rounded-t-lg",
     },
     title: {
-      base: "flex w-full items-center justify-between first:rounded-t-lg last:rounded-b-lg px-5 text-left font-medium text-gray-500 dark:text-gray-400",
+      base: "flex w-full items-center justify-between first:rounded-t-lg last:rounded-b-lg px-5 text-left font-medium text-gray-500",
       flush: {
-        off: "hover:bg-peach-100 dark:hover:bg-gray-800 dark:focus:ring-gray-800",
-        on: "bg-peach-200 dark:bg-transparent",
+        off: "hover:bg-peach-100",
+        on: "bg-peach-200",
       },
       open: {
-        on: "text-stone-900 bg-peach-100 dark:bg-stone-800 dark:text-white",
+        on: "text-stone-900 bg-peach-100",
       },
     },
   },
