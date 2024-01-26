212f16cac1bb72199b28c9a6701dbf4a5c3b24b4
ikulan <ikulan12@gmail.com>
2024-01-26 00:49:40 -0800

    Add Education Card component

---
+++ src/components/EduCard.jsx
@@ -0,0 +1,45 @@
+import { Card } from "flowbite-react";
+
+const customTheme = {
+  root: {
+    base: "flex rounded-lg border border-gray-200 bg-white shadow-md items-center",
+    children: "flex h-full flex-col justify-center p-2 lg:p-4",
+    horizontal: {
+      off: "flex-col",
+      on: "flex-row",
+    },
+  },
+  img: {
+    base: "",
+    horizontal: {
+      off: "rounded-t-lg",
+      on: "w-16 md:w-20 lg:w-28 rounded-t-lg object-cover md:rounded-none md:rounded-l-lg",
+    },
+  },
+};
+
+function EduCard(props) {
+  return (
+    <Card
+      theme={customTheme}
+      imgSrc={props.logo}
+      className="max-w-full gap-1 px-4 py-2 md:max-w-xs md:py-4 lg:max-w-[26rem]"
+      horizontal
+    >
+      <div>
+        <p className="text-md font-semibold lg:text-lg">{props.degree}</p>
+        <p className="font-semibold lg:text-xl">{props.program}</p>
+        <p className="text-sm text-gray-500 lg:text-base">{props.school}</p>
+        <p className="text-sm text-gray-500 lg:text-base">{props.location}</p>
+        <p className="text-sm text-gray-500 lg:text-base">{props.years}</p>
+        {props.hasDetail && (
+          <a className="text-peach-600 hover:underline" href="">
+            Learn more
+          </a>
+        )}
+      </div>
+    </Card>
+  );
+}
+
+export default EduCard;
